a52148bb03de57c3e84b099c28cd033fd045fe94
dstnl <dstnl@users.noreply.github.com>
2022-04-21 12:32:51 +0300

    updated login form test

--- pages/login_page.py
+++ pages/login_page.py
@@ -15,9 +15,7 @@ class LoginPage(BasePage):
         assert True
 
     def should_be_login_form(self):
-        assert self.is_element_present(By.CSS_SELECTOR, *LoginPageLocators.LOGIN_FORM), "Login form is not presented"
-        assert True
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
 
     def should_be_register_form(self):
-        assert self.is_element_present(By.CSS_SELECTOR, *LoginPageLocators.REGISTER_FORM), "Login link is not presented"
-        assert True
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
